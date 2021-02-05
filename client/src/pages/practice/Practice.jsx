73da595d35071e90128c91aadd2c999c326e2621
Pushkar Choudhary <pushkar.choudhary.78@gmail.com>
2021-02-05 17:31:48 +0530

    Interview and Multer

--- client/src/pages/practice/Practice.jsx
+++ client/src/pages/practice/Practice.jsx
@@ -1,13 +1,63 @@
-import React from 'react';
+import React,{useEffect} from 'react'
+import { makeStyles } from "@material-ui/core/styles";
+import Card from '../../components/Card/Card'
+import { Grid } from '@material-ui/core';
+import {getTopics} from '../../store/actions/topics'
+import {useDispatch,useSelector} from 'react-redux' ;
+
 
 const useStyles = makeStyles({
-    
-});
+    ihead:{
+        marginBottom : "3rem",
+        "&>h1":{
+        fontWeight : "700",
+        fontSize : "28px",
+        "&>span":{
+            color : "#2272FF",
+            fontSize :"40px"     
+        },
+    },
+     "&>p":{
+            width : "50%",
+            minWidth : "400px"
+        }
+  },
+  container :{
+      maxWidth : "1500px",
+      margin : "10px auto",
+      padding : "10px",
+  },
+})
+function Practice() {
+    const classes = useStyles()  ;
+    const topics = useSelector(state => state.topics) ;
+    const dispatch = useDispatch()
+    useEffect(()=>{
+      dispatch(getTopics())
+    },[]) ;
 
-export default function Practice() {
+
     return (
-        <div>
-            Practice Page;
+        <div className={classes.container}>
+            <div className = {classes.ihead}>
+                        <h1><span>P</span>ractice</h1>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an un.
+                        </p>
+            </div>
+            <div className ={classes.topics}>
+                <Grid container spacing={100}> 
+                    {
+                        
+                        topics?.topics?.map(el=>{
+                
+                                return (<Grid item xs={12} sm={6} md={4} style={{marginBottom : "3rem"}}><Card topic={el}  /></Grid>)
+                        })
+                    }
+                 </Grid>
+            </div>
         </div>
     )
 }
+
+export default Practice
