05320921b3b8c049fe5ed1b4da158753b2baeecf
Daksh <dakshchhabra652@gmail.com>
2021-02-08 20:11:31 +0530

    netlify redirects 200

--- client/src/http/api.js
+++ client/src/http/api.js
@@ -1,3 +1,4 @@
 const baseUrl = 'https://interview-tracker-iitg.herokuapp.com'
+const localuRL = 'http://localhost:8080'
 
 export default baseUrl;
--- server/controllers/shakalaka.js
+++ server/controllers/shakalaka.js
@@ -2,14 +2,16 @@ const InterviewExp  = require('../models/Interview');
 const User  = require('../models/User');
 
 const host = 'https://interview-tracker-iitg.herokuapp.com'
-
+const local = 'http://localhost:3000'
 const jaadu1 = async (req,res)=>{
     const interviews = await InterviewExp.find({})
+    console.log('entered')
     //console.log(interviews)
     await Promise.all(
         interviews.map(async i=>{
-            if(i.image.contentType.includes('3001')){
-                i.image.contentType = i.image.contentType.replace(`${host}${host}`,host)
+            if(i.image.contentType.includes(host)){
+                i.image.contentType = i.image.contentType.replace(host,local)
+                console.log(i.image.contentType)
                 await i.save()
             }
         })
@@ -22,8 +24,8 @@ const jaadu2 = async (req,res)=>{
     const interviews = await User.find({})
     await Promise.all(
         interviews.map(async i=>{
-            if(i.image&&i.image.contentType&&i.image.contentType.includes('localhost')){
-                i.image.contentType = i.image.contentType.replace(`${host}${host}`,host)
+            if(i.image&&i.image.contentType&&i.image.contentType.includes(host)){
+                i.image.contentType = i.image.contentType.replace(host,local)
                 await i.save()
             }
         })
